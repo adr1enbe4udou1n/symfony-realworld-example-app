dbb51f3d3bb28e3c182edf60a7b6ef3e1efe76b0
Adrien Beaudouin <adrien.beaudouin@outlook.com>
2022-01-18 18:51:49 +0100

    fix reserved keyword

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -28,6 +28,7 @@ use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
 use Symfony\Component\Security\Core\User\UserInterface;
 
 #[ORM\Entity(repositoryClass: UserRepository::class)]
+#[ORM\Table(name: '`user`')]
 #[UniqueEntity('email', message: 'user.email.unique')]
 #[ApiResource(
     collectionOperations: [
