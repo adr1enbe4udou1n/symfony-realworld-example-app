18f7f880850a2000c0ebeb83953f418a5472bf25
Adrien Beaudouin <adrien.beaudouin@outlook.com>
2023-05-18 22:40:51 +0200

    remove celeb

--- src/Controller/ProfileController.php
+++ src/Controller/ProfileController.php
@@ -13,7 +13,7 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Http\Attribute\IsGranted;
 
-#[Route('/profiles/celeb_{username}')]
+#[Route('/profiles/{username}')]
 class ProfileController extends AbstractController
 {
     public function __construct(
